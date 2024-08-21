f77cc28beacb19b9f0a8d63b83596eea62161c84
prateektalukdar <108460893+prateektalukdar@users.noreply.github.com>
2024-08-21 13:10:53 +0530

    Add files via upload

---
+++ rpi5_final.sh
@@ -0,0 +1,6 @@
+cd whisper_cpp/
+./main -nt -m models/ggml-tiny.en.bin -f samples/test_audio2.wav | tee whisper_output.txt
+cd ../llama_cpp/build/bin/
+./llama-cli -m llama-2-7b.Q4_0.gguf -p "$(cat /home/myelin/Desktop/whisper_cpp/whisper_output.txt)" -n 32 | tee llama_output.txt
+cd ../../../piper_arm64/
+echo "$(cat /home/myelin/Desktop/llama_cpp/build/bin/llama_output.txt)" | ./piper --model en_US-amy-medium.onnx --output_file final_tts.wav
