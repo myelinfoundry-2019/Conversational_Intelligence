0e79e1641a94cbd982e6610038aa4278bf0481dc
prateektalukdar <108460893+prateektalukdar@users.noreply.github.com>
2024-08-23 14:28:00 +0530

    Updated 8295_final.sh with llama-2-chat

--- 8295_final.sh
+++ 8295_final.sh
@@ -2,7 +2,7 @@ cd whisper_cpp/
 ./main -nt -m models/ggml-tiny.en.bin -f samples/test_audio3.wav | tee whisper_output.txt
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/llama_cpp/build/src:/data/llama_cpp/build/ggml/src:/data/piper_arm64/
 cd ../llama_cpp/build/bin/
-./llama-cli -m llama-2-7b.Q4_0.gguf -p "$(cat /data/whisper_cpp/whisper_output.txt)" -n 32 | tee llama_output.txt
+./llama-cli -m llama-2-7b-chat.Q4_K_M.gguf -p "$(cat /data/whisper_cpp/whisper_output.txt)" -n 32 | tee llama_output.txt
 cd ../../../piper_arm64/
 echo "$(cat /data/llama_cpp/build/bin/llama_output.txt)" | ./piper --model en_US-amy-medium.onnx --output_file final_tts.wav
 aplay final_tts.wav
