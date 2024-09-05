3ee79767bf965ae34f5742a94b209c45b906c9a7
prateektalukdar <108460893+prateektalukdar@users.noreply.github.com>
2024-09-05 16:30:59 +0530

    Update 8295_stream_final.sh

--- 8295_stream_final.sh
+++ 8295_stream_final.sh
@@ -3,10 +3,10 @@ file1="/data/whisper_cpp/whisper_output.txt"
 file2="/data/llama_cpp/build/bin/llama_output.txt"
 
 cd whisper_cpp/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/whisper_cpp:/data/llama_cpp/build/src:/data/llama_cpp/build/ggml/src:/data/piper_arm64/
 timeout 4s ./stream -m models/ggml-tiny.en.bin --step 4000 --length 4000 -c 0 -t 4 -ac 512 | tee whisper_output.txt
 tail -c +$((127 + 2)) "$file1" > temp_file && mv temp_file "$file1"
 char_count=$(wc -m < "$file1")
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/data/llama_cpp/build/src:/data/llama_cpp/build/ggml/src:/data/piper_arm64/
 cd ../llama_cpp/build/bin/
 ./llama-cli -m llama-2-7b-chat.Q4_K_M.gguf -p "$(cat /data/whisper_cpp/whisper_output.txt)" -n 64 | tee llama_output.txt
 tail -c +$((char_count + 2)) "$file2" > temp_file && mv temp_file "$file2"
